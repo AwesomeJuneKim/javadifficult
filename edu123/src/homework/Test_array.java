15336ac28a1c9e0b4d53c9f5c59973af30fad6dd
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-02-15 16:05:53 +0900

    homework

--- edu123/src/homework/Test_array.java
+++ edu123/src/homework/Test_array.java
@@ -1,6 +1,8 @@
 package homework;
 
-public class T {
+import java.util.Random;
+
+public class Test_array {
 	/*
 	 * 하나의 자바 파일에서 함수 구현 실습
 	 */
@@ -16,10 +18,7 @@ public class T {
 				}
 			}
 		}
-		static void getData(int[][]b){
-			//static을 안붙이고 싶으면 메인메소드에 클래스의 참조변수를 연결해줘야 한다.
-			
-		}
+
 
 		static int[][] addMatrix(int[][]B, int[][]C){
 			int m[][];
@@ -27,22 +26,51 @@ public class T {
 			int c=B[0].length;
 			m = new int[r][c];
 			for(int i=0;i<r;i++)
-				for(int j=0; i<c;j++)
+				for(int j=0; j<c;j++)
 					m[i][j]=B[i][j]+C[i][j];
 			return m;
 		}
-		multiplyMatrix(B,E){
+		static int [][] multiplyMatrix(int[][]B,int[][]E){
+			int m[][];
+			int r=B.length;
+			int c=B[0].length;
+			int n=E[0].length;
+			m= new int[r][n];
+			for(int i=0;i<r;i++)
+				for(int j=0; j<n;j++)
+					for(int k=0;k<c;k++)
+					m[i][j]+=B[i][k]*E[k][j];
+			return m;
 			
 		}
-		transposeMatrix(B){
-			
+		static int [][] transposeMatrix(int[][]B){//전치행렬
+			int m[][];
+			int r=B.length;
+			int c=B[0].length;
+			m = new int[r][c];
+			for(int i=0;i<r;i++)
+				for(int j=0;j<c;j++) 
+					m[j][i]=B[i][j];
+			return m;
 		}
-
+		static void getData(int[][]X) {
+			Random rnd = new Random();
+			for(int i=0;i<X.length;i++)
+				for(int j=0;j<X[0].length;j++)
+					X[i][j]=rnd.nextInt(10);
+		}
 		public static void main(String[] args) {
 			/*
 			 * 난수 생성으로 초기화 A[3][4] = B[3][4] + C[3][4]; D[3][5] = B[3][4] * E[4][5]; F[4][3]
 			 * = B[3][4]의 전치 행렬
 			 */
+			int [][]A=new int [3][4];
+			int [][]B=new int [3][4];
+			int [][]C=new int [3][4];
+			int [][]D=new int [3][5];
+			int [][]E=new int [4][5];
+			int [][]F=new int [4][3];
+			
 			System.out.println("행렬 더하기: A[3][4] = B[3][4] + C[3][4]");
 			getData(B);
 			getData(C);
