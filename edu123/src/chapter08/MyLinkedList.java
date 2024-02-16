f68af2797d285d28d479cf419be0ed605b3ce60d
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-02-16 11:52:31 +0900

    update

--- edu123/src/chapter08/MyLinkedList.java
+++ edu123/src/chapter08/MyLinkedList.java
@@ -2,4 +2,22 @@ package chapter08;
 
 public class MyLinkedList {
 
+	private Node head;
+	private class Node{
+		private String data;
+		private Node link;
+		
+		public Node(String data) {
+			this.data=data; link=null;// 원래하던 방식?
+		}
+	}
+	public void add(String data) {
+		Node newNode= new Node(data);
+		if(head==null)
+			head= newNode;
+	}
+	public MyLinkedList(Node head) {
+		this.head=head;
+	}
+
 }
--- edu123/src/chapter08/MyLinkedTest.java
+++ edu123/src/chapter08/MyLinkedTest.java
@@ -1,5 +1,9 @@
 package chapter08;
 
 public class MyLinkedTest {
-
+public static void main(String[] args) {
+	MyLinkedList myList=new MyLinkedList();
+	myList.add("Hong");
+	// myList.print();
+}
 }
