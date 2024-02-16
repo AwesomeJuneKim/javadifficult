5d5abe0fa44f3e896b4f38ec8b6530f10c899cd6
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-02-16 12:30:38 +0900

    ProfHong4th

--- edu123/src/chapter08/MyLinkedList.java
+++ edu123/src/chapter08/MyLinkedList.java
@@ -8,13 +8,24 @@ public class MyLinkedList {
 		private Node link;
 		
 		public Node(String data) {
-			this.data=data; link=null;// 원래하던 방식?
+			//해당 생성자는 필드가 2개 이므로 heap에 필드공간 2개를 할당한다.
+			this.data=data; link=null;
 		}
 	}
 	public void add(String data) {
 		Node newNode= new Node(data);
 		if(head==null)
 			head= newNode;
+		else {
+			Node next=head;
+			while(next.link!=null) {
+				next=next.link;
+			}
+			next.link=newNode;
+		}
+	}
+	public void print() {
+		
 	}
 	public MyLinkedList(Node head) {
 		this.head=head;
