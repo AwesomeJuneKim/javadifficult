24952b94617b607b4a020ef6add3b2ad80bda24f
JuneKim <junekim@localhost>
2024-02-18 16:06:23 +0900

    Homework updated2/18

--- edu123/src/chapter07/Test_Chap06_행렬클래스.java
+++ edu123/src/chapter07/Test_Chap06_행렬클래스.java
@@ -1,67 +1,68 @@
 package chapter07;
 
 import java.util.Random;
+
 //homework package의 T3와 같은 파일 
 /*
 	 * 클래스 메소드 구현 실습 목적
 	 */
-	class Matrix {
-		int rows;
-		int cols;
-		int[][] data;
+class Matrix {
+	int rows;
+	int cols;
+	int[][] data;
 
-		public Matrix(int rows, int cols) {
-			this.rows = rows;
-			this.cols = cols;
-			data = new int[rows][cols];
-		}
+	public Matrix(int rows, int cols) {
+		this.rows = rows;
+		this.cols = cols;
+		data = new int[rows][cols];
+	}
 
-		public void getData() {
-			// 난수로 입력
-			Random rnd=new Random();
-			for(int i=0; i<this.rows;i++)
-				for(int j=0;j<this.cols;j++)
-					this.data[i][j]=rnd.nextInt(10);
-		
-		}
+	public void getData() {
+		// 난수로 입력
+		Random rnd = new Random();
+		for (int i = 0; i < this.rows; i++)
+			for (int j = 0; j < this.cols; j++)
+				this.data[i][j] = rnd.nextInt(10);
 
-		Matrix addMatrix(Matrix b) {
-			Matrix m = new Matrix(this.rows, this.cols);
-			for(int i=0; i<this.rows; i++)
-				for(int j=0; j<this.cols;j++)
-					m.data[i][j]=this.data[i][j]+b.data[i][j];
-			return m;
-		}
+	}
 
-		Matrix multiplyMatrix(Matrix b) {
-			Matrix m = new Matrix(this.rows, b.cols);
-			for(int i=0; i<this.rows;i++)
-				for(int j=0;j<this.cols;j++)
-					for(int k=0;k<this.cols;k++)
-						m.data[i][j]+=this.data[i][k]*b.data[k][j];
-			return m;
-		}
+	Matrix addMatrix(Matrix b) {
+		Matrix m = new Matrix(this.rows, this.cols);
+		for (int i = 0; i < this.rows; i++)
+			for (int j = 0; j < this.cols; j++)
+				m.data[i][j] = this.data[i][j] + b.data[i][j];
+		return m;
+	}
 
-		Matrix transposeMatrix() {
-			Matrix m = new Matrix(this.cols, this.rows);
-				for(int i=0; i<this.cols;i++)
-					for(int j=0; j<this.rows;j++)
-						m.data[i][j]=this.data[j][i];
-			return m;
-		}
+	Matrix multiplyMatrix(Matrix b) {
+		Matrix m = new Matrix(this.rows, b.cols);
+		for (int i = 0; i < this.rows; i++)
+			for (int j = 0; j < this.cols; j++)
+				for (int k = 0; k < this.cols; k++)
+					m.data[i][j] += this.data[i][k] * b.data[k][j];
+		return m;
+	}
 
-		void showMatrix(String str, int[][]B) {
-			System.out.println(str);
-			for(int i=0;i<B.length;i++) {
-				for(int j=0; j<B[i].length;j++) {
-					System.out.print(B[i][j]+" ");
-			// 2차원 배열 모양으로 출력하는 코드 작성
-				}
+	Matrix transposeMatrix() {
+		Matrix m = new Matrix(this.cols, this.rows);
+		for (int i = 0; i < this.cols; i++)
+			for (int j = 0; j < this.rows; j++)
+				m.data[i][j] = this.data[j][i];
+		return m;
+	}
+
+	void showMatrix(String str) {
+		System.out.println(str);
+		for (int i = 0; i < this.rows; i++) {
+			for (int j = 0; j < this.cols; j++) {
+				System.out.print(this.data[i][j] + " ");
+				// 2차원 배열 모양으로 출력하는 코드 작성
+			}
 			System.out.println();
 		}
 
-		
 	}
+}
 
 	public class Test_Chap06_행렬클래스 {
 
@@ -81,23 +82,22 @@ import java.util.Random;
 			E.getData();
 
 			A = B.addMatrix(C);
-			B.showMatrix("B[3][4]",B.data);
-			C.showMatrix("C[3][4]",C.data);
-			A.showMatrix("A[3][4]",A.data);
+			B.showMatrix("B[3][4]");
+			C.showMatrix("C[3][4]");
+			A.showMatrix("A[3][4]");
 
 			System.out.println("행렬 곱하기: D[3][5] = B[3][4] * E[4][5]");
 			D = B.multiplyMatrix(E);
-			B.showMatrix("B[3][4]",B.data);
-			E.showMatrix("E[4][5]",E.data);
-			D.showMatrix("D[3][5]",D.data);
+			B.showMatrix("B[3][4]");
+			E.showMatrix("E[4][5]");
+			D.showMatrix("D[3][5]");
 
 			System.out.println("행렬 전치: F[4][3] = B[3][4]의 전치 행렬");
 			F = B.transposeMatrix();
-			B.showMatrix("B[3][4]", B.data);
-			F.showMatrix("F[4][3]", F.data);
+			B.showMatrix("B[3][4]");
+			F.showMatrix("F[4][3]");
 
 		}
 
 	}
-
 
---
+++ edu123/src/homework/Homework3.java
@@ -0,0 +1,83 @@
+package homework;
+
+import java.util.Random;
+
+public class Homework3 {
+	/*
+	 * 하나의 자바 파일에서 함수 구현 실습
+	 */
+
+	/*
+	 * 행렬 덧셈, 곱셈 연산 메소드 정의 1차버젼: 덧셈후 바로 출력 2차버젼: 덧셈후 return 값으로 결과 행렬 반환
+	 */
+	static void showMatrix(int[][] X) {
+		// 2차원 모습으로 출력
+		for (int i = 0; i < X.length; i++) {
+			for (int j = 0; j < X.length; j++) {
+				System.out.println(X[i][j] + " ");
+			}
+		}
+	}
+
+	public void getData(int[][]b) {
+		// static을 안붙이고 싶으면 메인메소드에 클래스의 참조변수를 연결해줘야 한다.
+		Random rnd=new Random();
+		for(int i=0;i<b.length;i++)
+			for(int j=0;j<b[i].length;j++)
+			b[i][j]=rnd.nextInt(10);
+
+	}
+
+	public int[][] addMatrix(int[][]B, int[][]C){
+		int[][]m=new int[B.length][B[0].length];
+				for(int i=0;i<B.length;i++)
+					for(int j=0;j<B[i].length;j++)
+						m[i][j]=B[i][j]+C[i][j];
+				return m;
+			}
+
+	public static Matrix multiplyMatrix(Matrix a, Matrix b) {
+		Matrix result= new Matrix(a.rows, b.cols);
+		for(int i=0; i<a.rows;i++)
+			for(int j=0;j<b.cols;j++)
+				for(int k=0;k<a.cols;k++)
+					result data[i][j]+= a.data[i][k]*b.data[k][j];
+		return result;
+					
+
+	}
+
+	static int[][] transposeMatrix(int[][] B) {
+
+	}
+
+	public static void main(String[] args) {
+		/*
+		 * 난수 생성으로 초기화 A[3][4] = B[3][4] + C[3][4]; D[3][5] = B[3][4] * E[4][5]; F[4][3]
+		 * = B[3][4]의 전치 행렬
+		 */
+		int[][]B= new int[3][4];
+		int[][]C=new int[3][4];
+		int[][]E= new int[3][4];
+		
+		System.out.println("행렬 더하기: A[3][4] = B[3][4] + C[3][4]");
+		getData(B);
+		getData(C);
+		getData(E);
+		A = addMatrix(B, C);
+		showMatrix(B);
+		showMatrix(C);
+		showMatrix(A);
+		System.out.println("행렬 곱하기: D[3][5] = B[3][4] * E[4][5]");
+		D = multiplyMatrix(B, E);
+		showMatrix(B);
+		showMatrix(E);
+		showMatrix(D);
+		System.out.println("행렬 전치: F[4][3] = B[3][4]의 전치 행렬");
+		F = transposeMatrix(B);
+		showMatrix(B);
+		showMatrix(F);
+
+	}김우정이 다해라!!!
+
+}
