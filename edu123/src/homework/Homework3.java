4e6e290a107227e6a693dd3d1bae93d66dd6e316
JuneKim <junekim@localhost>
2024-02-18 16:54:55 +0900

    exist static

--- edu123/src/homework/Homework3.java
+++ edu123/src/homework/Homework3.java
@@ -13,22 +13,22 @@ public class Homework3 {
 	static void showMatrix(int[][] X) {
 		// 2차원 모습으로 출력
 		for (int i = 0; i < X.length; i++) {
-			for (int j = 0; j < X.length; j++) {
-				System.out.println(X[i][j] + " ");
+			for (int j = 0; j < X[i].length; j++) {
+				System.out.print(X[i][j] + " ");
 			}
+			System.out.println();
 		}
 	}
-
-	public void getData(int[][]b) {
+	
+	static void getData(int[][]B) {
 		// static을 안붙이고 싶으면 메인메소드에 클래스의 참조변수를 연결해줘야 한다.
 		Random rnd=new Random();
-		for(int i=0;i<b.length;i++)
-			for(int j=0;j<b[i].length;j++)
-			b[i][j]=rnd.nextInt(10);
-
+		for(int i=0;i<B.length;i++)
+			for(int j=0;j<B[i].length;j++)
+			B[i][j]=rnd.nextInt(10);
 	}
 
-	public int[][] addMatrix(int[][]B, int[][]C){
+	static int[][] addMatrix(int[][]B, int[][]C){
 		int[][]m=new int[B.length][B[0].length];
 				for(int i=0;i<B.length;i++)
 					for(int j=0;j<B[i].length;j++)
@@ -36,18 +36,23 @@ public class Homework3 {
 				return m;
 			}
 
-	public static Matrix multiplyMatrix(Matrix a, Matrix b) {
-		Matrix result= new Matrix(a.rows, b.cols);
-		for(int i=0; i<a.rows;i++)
-			for(int j=0;j<b.cols;j++)
-				for(int k=0;k<a.cols;k++)
-					result data[i][j]+= a.data[i][k]*b.data[k][j];
-		return result;
+	static int[][] multiplyMatrix(int[][]B, int[][]E) {
+		int[][]D= new int [B.length][E[0].length];
+		for(int i=0; i<B.length;i++)
+			for(int j=0;j<E[0].length;j++)
+				for(int k=0;k<B[0].length;k++)
+					D[i][j]+= B[i][k]*E[k][j];
+		return D;
 					
 
 	}
 
 	static int[][] transposeMatrix(int[][] B) {
+		int[][]F= new int[B[0].length][B.length];
+		for(int i=0;i<B.length;i++)
+			for(int j=0; j<B[0].length;j++)
+				F[j][i]=B[i][j];
+		return F;
 
 	}
 
@@ -58,23 +63,23 @@ public class Homework3 {
 		 */
 		int[][]B= new int[3][4];
 		int[][]C=new int[3][4];
-		int[][]E= new int[3][4];
+		int[][]E= new int[4][5];
 		
 		System.out.println("행렬 더하기: A[3][4] = B[3][4] + C[3][4]");
 		getData(B);
 		getData(C);
 		getData(E);
-		A = addMatrix(B, C);
+		int[][]A = addMatrix(B, C);
 		showMatrix(B);
 		showMatrix(C);
 		showMatrix(A);
 		System.out.println("행렬 곱하기: D[3][5] = B[3][4] * E[4][5]");
-		D = multiplyMatrix(B, E);
+		int[][]D = multiplyMatrix(B, E);
 		showMatrix(B);
 		showMatrix(E);
 		showMatrix(D);
 		System.out.println("행렬 전치: F[4][3] = B[3][4]의 전치 행렬");
-		F = transposeMatrix(B);
+		int[][]F = transposeMatrix(B);
 		showMatrix(B);
 		showMatrix(F);
 
