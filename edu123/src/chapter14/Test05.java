e515695918f297aee8ba16a6b8fb5169c9efa687
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-02-28 17:19:34 +0900

    람다식을 이용한 합

--- edu123/src/chapter14/Test05.java
+++ edu123/src/chapter14/Test05.java
@@ -14,7 +14,17 @@ public static void main(String[] args) {
 		}
 		return result;
 	};
-	System.out.println("1부터 10까지의 합 : "+sum.func(10));
-	System.out.println("1부터 100까지의 합 : "+sum.func(100));
+	NumberFunc osum=(n)->{
+		int result=0;
+		for(int i=0; i<=n;i++) {
+			if(i%2==1) {
+				result +=i;
+			}
+		}
+		return result;
+	};
+	System.out.println("1부터 10까지의 짝수의 합 : "+sum.func(10));
+	System.out.println("1부터 100까지의 짝수의 합 : "+sum.func(100));
+	System.out.println("1부터 10까지 홀수의 합 : "+osum.func(10));
 }
 }
