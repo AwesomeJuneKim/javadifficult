31138ccffc4dceb1744c120d3ead629cf266b3bb
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-02-29 11:51:44 +0900

    주석만 남기고 코드는 모두 삭제 후 복습

--- edu123/src/selfStudy/FunctionP.java
+++ edu123/src/selfStudy/FunctionP.java
@@ -2,10 +2,11 @@ package selfStudy;
 
 import java.util.function.Function;
 
+//최상위 추상클래스 생성(추상메서드로 면적 구하는 메서드 하나 선언)
 abstract class Geometry {
 	abstract double getArea();
 }
-
+//자식 클래스들 생성(원, 삼각형, 직사각형)
 class Circle extends Geometry {
 	int radius;
 
@@ -52,9 +53,11 @@ class Rectangle extends Geometry {
 
 public class FunctionP {
 	public static void main(String[] args) {
+		//Function 람다생성
 		Function<Geometry, Double> func = (g) -> { // Function이라는 API를 사용함
 			return g.getArea();
 		};
+		//출력
 		System.out.println("원 면적 : " + func.apply(new Circle(5)));
 		System.out.println("삼각형 면적 : " + func.apply(new Triangle(5, 10)));
 		System.out.println("사각형 면적 : " + func.apply(new Rectangle(6, 12)));
