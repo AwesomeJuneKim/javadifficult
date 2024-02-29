81a1a8b918d704583f9e4471a4af908ed6b42535
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-02-29 11:47:42 +0900

    교수님 정답

--- edu123/src/selfStudy/FunctionP.java
+++ edu123/src/selfStudy/FunctionP.java
@@ -2,51 +2,63 @@ package selfStudy;
 
 import java.util.function.Function;
 
-class Geometry{}
-class Circle extends Geometry{
+abstract class Geometry {
+	abstract double getArea();
+}
+
+class Circle extends Geometry {
 	int radius;
+
 	public Circle(int radius) {
-		super();
-		this.radius=radius;
+		this.radius = radius;
 	}
-	public double calculator(){
-		return radius*radius*3.14;
-		
+
+	@Override
+	double getArea() {
+		return radius * radius * Math.PI;
 	}
-	
+
 }
-class Triangle extends Geometry{
+
+class Triangle extends Geometry {
 	int width;
 	int height;
+
 	public Triangle(int width, int height) {
-		super();
-		this.width=width;
-		this.height=height;
-	}
-	public double calculator() {
-		return width*height;
-	}
-}
-class Rectangle extends Geometry{
-	int width;
-	int height;
-	
-	public Rectangle(int width, int height) {
-		super();
 		this.width = width;
 		this.height = height;
 	}
-	public double calculator() {
-		return width*height;
+
+	@Override
+	double getArea() {
+		return (double) width * height;
 	}
 }
 
+class Rectangle extends Geometry {
+	int width;
+	int height;
 
-public class FunctionP extends Geometry{
+	public Rectangle(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+
+	@Override
+	double getArea() {
+		return (double) width * height * 0.5;
+	}
+}
+
+public class FunctionP {
 	public static void main(String[] args) {
-		Function<Geometry, Double> func=(g)->{
-			
+		Function<Geometry, Double> func = (g) -> { // Function이라는 API를 사용함
+			return g.getArea();
 		};
+		System.out.println("원 면적 : " + func.apply(new Circle(5)));
+		System.out.println("삼각형 면적 : " + func.apply(new Triangle(5, 10)));
+		System.out.println("사각형 면적 : " + func.apply(new Rectangle(6, 12)));
+
 	}
 
 }
