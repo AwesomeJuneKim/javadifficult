20eeb6480b56d98d56c541e4e7feb72127203762
JuneKim <junekim@localhost>
2024-03-04 00:01:30 +0900

    구구단 복습했는데 이상

--- edu123/src/selfStudy/Gugudan0.java
+++ edu123/src/selfStudy/Gugudan0.java
@@ -2,27 +2,31 @@ package selfStudy;
 
 
 public class Gugudan0 {
-
-	public void printColumn(int col) {
-        int numberOfColumns = 3; // 한 행에 출력할 열의 수
-
-        for (int i = 1; i <= 9; i++) {
-            for (int j = 0; j < numberOfColumns; j++) {
-                int dan = col + j;
-                if (dan <= 9) {
-                    System.out.print(dan + "x" + i + "=" + (i * dan) + "\t");
-                } else {
-                    break; // 9단을 넘어가면 종료
-                }
-            }
-            System.out.println();
-        }
-    }
-
-    public static void main(String[] args) {
-        Gugudan ggd = new Gugudan();
-        ggd.printColumn(5); // 5단부터 출력합니다.
-
-        System.out.println("END");
-    }
+		
+	public static void main(String[] args) {
+		Gugudan0 ggd= new Gugudan0();
+		ggd.printColumn(3);
+		
+		System.out.println("END");
 	}
+	public void printColumn(int col) {
+	    int count = 0; // 출력된 구구단의 개수를 기록하는 변수
+
+	    for (int i = 1; i <= 9; i++) {
+	        for (int j = 0; j < col; j++) {
+	            int current = i + j * col;
+	            if (current <= 9) {
+	                System.out.print(current + " x " + col + " = " + (current * col) + "\t");
+	            }
+	        }
+
+	        // 구구단이 출력될 때마다 count를 증가시키고, 3의 배수일 때 빈 줄을 출력
+	        count++;
+	        if (count % 3 == 0) {
+	            System.out.println(); // 빈 줄 출력
+	        }
+	    }
+	}
+
+	}
+
